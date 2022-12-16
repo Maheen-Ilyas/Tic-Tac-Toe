b34494366657ec5aefac78b1df7f169410c4a0c2
Maheen-Ilyas <mahilyas05@gmail.com>
2022-12-17 00:35:53 +0530

    Step 3

---
+++ tictactoe/lib/gameboard.dart
@@ -0,0 +1,13 @@
+class Player {
+  static const x = "X";
+  static const o = "O";
+  static const empty = "";
+}
+
+class Game {
+  static const boardCount = 9;
+  static const blockSize = 100.0;
+  List<String>? board;
+  static List<String>? initGameBoard() =>
+      List.generate(boardCount, (index) => Player.empty);
+}
--- tictactoe/lib/main.dart
+++ tictactoe/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:tictactoe/colors.dart';
+import 'package:tictactoe/gameboard.dart';
 
 void main() {
   runApp(const MyApp());
@@ -13,33 +14,85 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: ThemeData(fontFamily: "Aquire"),
-      home: const Game(),
+      home: const GameScreen(),
     );
   }
 }
 
-class Game extends StatefulWidget {
-  const Game({super.key});
+class GameScreen extends StatefulWidget {
+  const GameScreen({super.key});
 
   @override
-  State<Game> createState() => _GameState();
+  State<GameScreen> createState() => _GameScreenState();
 }
 
-class _GameState extends State<Game> {
+class _GameScreenState extends State<GameScreen> {
+  String lastValue = "X";
+  Game game = Game();
+
+  @override
+  void initState() {
+    super.initState();
+    game.board = Game.initGameBoard();
+    print(game.board);
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      backgroundColor: MainColors.backgroundColor,
-      body: SafeArea(
+    double boardWidth = MediaQuery.of(context).size.width;
+    return Scaffold(
+        backgroundColor: MainColors.backgroundColor,
+        body: SafeArea(
           child: Center(
-        child: Text(
-          "Tic Tac Toe",
-          style: TextStyle(
-              fontFamily: "Aquire",
-              color: MainColors.secondaryColor,
-              fontSize: 30),
-        ),
-      )),
-    );
+            child: Container(
+              padding: const EdgeInsets.all(16),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const Text(
+                    "Tic Tac Toe",
+                    style: TextStyle(
+                      fontFamily: "Aquire",
+                      color: MainColors.secondaryColor,
+                      fontSize: 40,
+                      fontWeight: FontWeight.w700,
+                    ),
+                  ),
+                  const SizedBox(height: 10),
+                  Text(
+                    ("It is $lastValue turn!").toUpperCase(),
+                    style: const TextStyle(
+                      color: MainColors.accentColor,
+                      fontSize: 20,
+                      fontFamily: "Aquire",
+                      fontWeight: FontWeight.w300,
+                    ),
+                  ),
+                  const SizedBox(height: 20),
+                  Container(
+                    padding: const EdgeInsets.all(16),
+                    width: boardWidth,
+                    height: boardWidth,
+                    child: GridView.count(
+                      crossAxisCount: Game.boardCount ~/ 3,
+                      mainAxisSpacing: 8,
+                      crossAxisSpacing: 8,
+                      children: List.generate(
+                          Game.boardCount,
+                          (index) => Container(
+                                width: Game.blockSize,
+                                height: Game.blockSize,
+                                decoration: BoxDecoration(
+                                    color: MainColors.secondaryColor,
+                                    borderRadius: BorderRadius.circular(5)),
+                              )),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ));
   }
 }
