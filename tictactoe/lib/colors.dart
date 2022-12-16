738596a049b4fa067dbff29ebfacf65d83cf1b89
Maheen-Ilyas <mahilyas05@gmail.com>
2022-12-16 22:39:21 +0530

    Step 1

---
+++ tictactoe/lib/colors.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/animation.dart';
+
+class MainColors {
+  static const backgroundColor = Color(0xFF008080);
+  static const secondaryColor = Color(0xFF483C32);
+  static const accentColor = Color(0xFFFFFFFF);
+}
---
+++ tictactoe/lib/main.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:tictactoe/colors.dart';
+
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(fontFamily: "Aquire"),
+      home: const Game(),
+    );
+  }
+}
+
+class Game extends StatefulWidget {
+  const Game({super.key});
+
+  @override
+  State<Game> createState() => _GameState();
+}
+
+class _GameState extends State<Game> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      backgroundColor: MainColors.backgroundColor,
+    );
+  }
+}
